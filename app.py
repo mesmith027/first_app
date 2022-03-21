11a3a2147ff031524d43946e919d615ec7e52f2f
Marisa Smith <marisa@streamlit.io>
2022-03-21 09:58:41 -0400

    first verion of Streamlit app finished

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.title("Creating my first Streamlit app")
 
-st.write("helllooo world!!! :smile:")
+# reading in the data from github
+df = pd.read_csv("https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv")
 
-click = st.button("Press me")
+#setting up the sidebar
+with st.sidebar:
+    st.write("Widget to control the app:")
+    check = st.checkbox("View raw data")
 
-if click:
-    st.balloons()
+# selectbox that controls the pie graph
+option = ['Region','Category', 'Segment','Ship Mode']
+pick = st.sidebar.selectbox("Choose an option", option)
 
-check = st.checkbox("Checkbox here")
+# creating the pie figure
+st.write("""
+## Pie chart :pie:
 
-if check:
-    st.snow()
+Choose between `Region`, `Category`, `Segment` and `Ship Mode` on the selectbox
+in the sidebar to show the Quantity based on those features!
+    """)
+fig = px.pie(df, values = "Quantity", names=pick, title="Quantity by {}".format(pick))
+st.plotly_chart(fig)
 
-option = ["Name","Telephone","Address","Email"]
+# slider that controls our histogram
+max = int(round(df["Sales"].max()))
+value = st.sidebar.slider("My first slider", 0,max, max)
 
-pick = st.selectbox("Choose an option", option)
+# creating our histogram
+st.write("""
+## Histogram of Sales :moneybag:
 
-pick
+Here is a standard histogram binning the number of sales by their amount.
 
-value = st.slider("My first slider", 150,500)
-value
+The largest number of the sales are for amounts of \$25 and below. Only a few
+sales are larger than \$10,000, and only one larger than \$20,000.
+        """)
+fig = px.histogram(df, x="Sales", range_x=[0,value])
+st.plotly_chart(fig)
